12b90667dfe73c8e3f47949f3c135783fb119970
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2024-09-17 15:34:30 -0700

    done

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-let textBox = document.getElementById("addNewText")
+let textBox = document.getElementById("addNewText") // input box
 
-const addNew = document.getElementById("addNew")
-const container = document.getElementById("container")
+const addNew = document.getElementById("addNew") // + button
+const container = document.getElementById("container") // location for tasks to appear
 
-addNew.addEventListener("click", listItems)
+addNew.addEventListener("click", listItems) // calls the listItems function when the + is clicked
 
 
 function listItems(){
-    let textBoxValue = document.getElementById("addNewText").value
+    let textBoxValue = document.getElementById("addNewText").value // sets the textbox to a value
 
-    if(textBoxValue === ""){
+    if(textBoxValue === ""){ // if the entry is clear, leave the function
         console.log("clear")
         return
     }
-    else{
-        const newElement = document.createElement("div")
-        const childLabel = document.createElement("label")
-        const buttonOne = document.createElement("button")
-        const buttonTwo = document.createElement("button")
+    else{ // Add new rows for tasks
+        const newElement = document.createElement("div") // row container
+        const childLabel = document.createElement("label") // task name
+        const buttonOne = document.createElement("button") // finish button
+        const buttonTwo = document.createElement("button") // delete button
 
+        // adds children to the row
         newElement.appendChild(childLabel)
         newElement.appendChild(buttonOne)
         newElement.appendChild(buttonTwo)
 
+        // gives classes and ids to the children
         buttonOne.classList.add("rowButtonFinished", "rowButton")
         buttonTwo.classList.add("rowButtonDelete", "rowButton")
         buttonOne.id = "finishButton"
@@ -30,24 +32,28 @@ function listItems(){
         childLabel.classList.add("child")
         newElement.classList.add("row")
 
+        // adds content to the elements being added ^^^
         childLabel.innerText = textBoxValue
         buttonOne.textContent = "Finished"
         buttonTwo.textContent = "Delete"
 
+        // puts the row inside of the premade container
         const container = document.getElementById("container")
         container.appendChild(newElement)
         
+        // once a task is submitted, clear the input field
         document.getElementById("addNewText").value = ""
     }
         
+        // calls the function when the respective button is pressed
         finishButton.addEventListener("click", finishTask)
         deleteButton.addEventListener("click", deleteTask)
 }
-function finishTask(event){
+function finishTask(event){ // crosses out a task
     const row = event.target.previousElementSibling
     row.classList.add("finished")
 }
-function deleteTask(event){
+function deleteTask(event){ // deletes a row
     const row = event.target.parentElement
     row.remove()
 }
